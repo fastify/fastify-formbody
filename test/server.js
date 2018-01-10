0c17023bf0dfd0ddc25054049e7df4137c6bf644
James Sumners <james.sumners@gmail.com>
2018-01-10 09:20:39 -0500

    Initial stream test (failing)

---
+++ test/server.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const fastify = require('fastify')()
+
+fastify
+  .register(require('../'), {bodyLimit: 500})
+  .post('/', function (req, reply) {
+    reply.send({success: true})
+  })
+
+fastify.listen(0, (err) => {
+  if (err) {
+    process.send({error: err})
+    process.exit(1)
+  }
+
+  process.send({port: fastify.server.address().port})
+  process.on('message', (m) => {
+    if (m === 'stop') fastify.close()
+  })
+})
