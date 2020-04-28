28e07bc89f9a794c15bdac068ae69edb78415bc8
Peter Balazs <frikille@gmail.com>
2020-04-28 13:04:30 +0100

    update type test

--- formbody.test-d.ts
+++ formbody.test-d.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
-import formBodyPlugin from './formbody'
+import formBodyPlugin, { FormBodyPluginOptions } from './formbody'
 
 const app = fastify()
 app.register(formBodyPlugin)
-app.register(formBodyPlugin, {})
-app.register(formBodyPlugin, {
+
+const emptyOpts: FormBodyPluginOptions = {}
+app.register(formBodyPlugin, emptyOpts)
+
+const opts: FormBodyPluginOptions = {
   bodyLimit: 1000
-})
+}
+app.register(formBodyPlugin, opts)
