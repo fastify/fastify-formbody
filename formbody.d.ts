c11fcb0b9c5b2e96cbc362569afb248ec02d7ca7
Eko Eryanto <ekoeryanto@gmail.com>
2021-02-06 16:57:39 +0700

    add parser to types

--- formbody.d.ts
+++ formbody.d.ts
@@ -2,6 +2,7 @@ import { FastifyPlugin } from 'fastify'
 
 export interface FormBodyPluginOptions {
   bodyLimit?: number
+  parser?: (str: string) => string
 }
 
 declare const formBodyPlugin: FastifyPlugin<FormBodyPluginOptions>
